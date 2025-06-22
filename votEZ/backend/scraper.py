9731ae1e3baa1df2f561bb74ea823f73fd19b5bc
Varun Chakka <vroomc08@gmail.com>
2025-06-21 20:08:01 -0700

    Finished Scraping + Adding everything to backend

--- votEZ/backend/scraper.py
+++ votEZ/backend/scraper.py
@@ -16,14 +16,28 @@ def scrape_election(state, position):
                 columns = tr.find_all('td')
                 if len(columns) > 0:
                     candidate_name = columns[2].get_text(strip=True)
-                    percentage = columns[3].get_text(strip=True)
                     results.append({
                         "candidate": candidate_name,
-                        "percentage": percentage
                     })
             return results
         except Exception as e:
             return {"error": str(e)}
     else:
         return {"error": "State/position not supported yet."}
+    
+def scrape_description(candidate_name):
+    url = f'https://ballotpedia.org/{candidate_name.replace(" ", "_")}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    try:
+        description = soup.find_all('div', {'style': 'height:400px;overflow:scroll;border:1px solid gray;margin:20px;padding:10px;'})
+        if not description:
+            return {"error": "Description not found"}
+        d = "".join([desc.get_text(strip=True) for desc in description])
+        return {"description": d}
+    except Exception as e:
+        return {"error": str(e)}
+#print(scrape_election("Hawaii", "Governor"))
+print(scrape_description("Joshua Green"))
 
